da3f3694194543d5c9e90f4bac71e54b0917b1e3
Strevia <phillipnissen7@gmail.com>
2022-10-04 16:23:44 -0400

    make it run

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
@@ -16,6 +16,7 @@ class TimelineActivity : AppCompatActivity() {
     lateinit var rvTweets: RecyclerView
     lateinit var adapter: TweetsAdapter
     lateinit var swipeContainer: SwipeRefreshLayout
+    var tweets = ArrayList<Tweet>()
 
 
 
